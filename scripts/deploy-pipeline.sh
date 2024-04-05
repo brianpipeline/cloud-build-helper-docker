a97a9170fc51f9748f1d51d25e6d4a059eab5980
Brian Kurilko <briankurilko@gmail.com>
2024-04-05 14:44:27 -0400

    Added pubsub topic creation to deploy-pipeline script

--- scripts/deploy-pipeline.sh
+++ scripts/deploy-pipeline.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 deployPipelines() {
+    projectId="$1"
+
     if ! (yq eval '.pipelineType' "pipeline.yaml" && yq eval '.pipelineName' "pipeline.yaml"); then
         echo "pipeline.yaml is missing pipelineType or pipelineName"
         exit 1
     fi
     pipelineName=$(yq eval '.pipelineName' "pipeline.yaml")
     pipelineType=$(yq eval '.pipelineType' "pipeline.yaml")
+    pubSubTopic=""
+    if [[ $pipelineType == "pubsub" ]]; then
+        if gcloud pubsub topics describe "$pipelineName" >/dev/null 2>&1; then
+            echo "Topic $pipelineName already exists."
+        else
+            echo "Creating topic $pipelineName"
+            if ! gcloud pubsub topics create "$pipelineName"; then
+                echo "Failed to create topic $pipelineName"
+                exit 1
+            fi
+        fi
+        pubSubTopic="--topic=projects/${projectId}/topics/$pipelineName"
+    fi
     substitutions=$(yq eval '.substitutions' "cloudbuild.yaml")
     substitutionsInOneLine=""
     for substitution in $substitutions; do
@@ -24,13 +39,13 @@ deployPipelines() {
         echo "Pipeline $pipelineName already exists. Updating."
         temp_file=$(mktemp)
         yq eval 'del(.substitutions)' "cloudbuild.yaml" >"$temp_file"
-        if ! (gcloud builds triggers update "$pipelineType" "$pipelineName" --region="us-central1" --clear-substitutions --inline-config="$temp_file" && gcloud builds triggers update "$pipelineType" "$pipelineName" --region="us-central1" --update-substitutions "$substitutionsInOneLine" --inline-config="$temp_file"); then
+        if ! (gcloud builds triggers update "$pipelineType" "$pipelineName" --region="us-central1" --clear-substitutions --inline-config="$temp_file" "$pubSubTopic" && gcloud builds triggers update "$pipelineType" "$pipelineName" --region="us-central1" --update-substitutions "$substitutionsInOneLine" --inline-config="$temp_file" "$pubSubTopic"); then
             echo "Failed to update pipeline $pipelineName"
             exit 1
         fi
     else
         echo "Creating pipeline $pipelineName"
-        if ! gcloud builds triggers create "$pipelineType" --name="$pipelineName" --secret="projects/212799175996/secrets/webhook-secret/versions/1" --region="us-central1" --inline-config="cloudbuild.yaml" --substitutions "$substitutionsInOneLine"; then
+        if ! gcloud builds triggers create "$pipelineType" --name="$pipelineName" --secret="projects/212799175996/secrets/webhook-secret/versions/1" --region="us-central1" --inline-config="cloudbuild.yaml" --substitutions "$substitutionsInOneLine" "$pubSubTopic"; then
             echo "Failed to create pipeline $pipelineName"
             exit 1
         fi
