e00f15b2dd62b1c30e1ed65d1052b4d21d721398
Brian Kurilko <briankurilko@gmail.com>
2024-04-11 12:06:56 -0400

    Added versioning to dockerfile deploy.

--- scripts/bats-tests/build-and-push-docker.bats
+++ scripts/bats-tests/build-and-push-docker.bats
@@ -24,12 +24,14 @@ teardown() {
 @test "buildAndPushDocker should successfully build and push" {
     # Stub gcloud builds submit command to return success
     stub docker "exit 0"
-    stub yq "exit 0"
+    stub yq "echo hello"
     # Run your function
-    run buildAndPushDocker "replyTopic" "project_id"
+    run buildAndPushDocker "replyTopic" "project_id" "refs/heads/main"
     # Check if it succeeds
     [ "$status" -eq 0 ]
-    [[ "$output" == *"Docker image built and pushed successfully."* ]]
+    echo "output: $output"
+    echo "hello"
+    [[ "$output" == *"Docker image us-central1-docker.pkg.dev/project_id/hello/hello:hello built and pushed successfully."* ]]
 }
 
 @test "buildAndPushDocker should exit 1 when docker build fails" {
@@ -37,8 +39,19 @@ teardown() {
     stub docker "exit 1"
     stub yq "exit 0"
     # Run your function
-    run buildAndPushDocker "replyTopic" "project_id"
+    run buildAndPushDocker "replyTopic" "project_id" "refs/heads/main"
     # Check if it succeeds
     [ "$status" -eq 1 ]
     [[ "$output" == *"Failed to build Docker image."* ]]
+}
+
+@test "buildAndPushDocker should push when on release branch" {
+    # Stub gcloud builds submit command to return success
+    stub docker "exit 0"
+    stub yq "echo hello"
+    # Run your function
+    run buildAndPushDocker "replyTopic" "project_id" "refs/heads/release/1.0"
+    # Check if it succeeds
+    [ "$status" -eq 0 ]
+    [[ "$output" == *"Docker image us-central1-docker.pkg.dev/project_id/hello/hello:1.0 built and pushed successfully."* ]]
 }
--- scripts/build-and-push-docker.sh
+++ scripts/build-and-push-docker.sh
@@ -5,20 +5,38 @@ source send-message.sh
 buildAndPushDocker() {
     replyTopic=$1
     projectId=$2
+    gitRef=$3
     repositoryName=$(yq eval '.repositoryName' "pipeline.yaml")
     artifactName=$(yq eval '.artifactName' "pipeline.yaml")
+    tag=$(yq eval '.tag' "pipeline.yaml")
 
-    if ! docker build -f Dockerfile -t us-central1-docker.pkg.dev/"${projectId}"/"$repositoryName"/"$artifactName":0.1 .; then
+    doNotPush=false
+    if [[ $gitRef == "refs/heads/main" ]]; then
+        if [[ $tag == "null" ]]; then
+            tag="latest"
+        fi
+    elif [[ $gitRef == *"release"* ]]; then
+        tag=$(echo "$gitRef" | cut -d'/' -f4)
+    else
+        doNotPush=true
+        tag="wontpush"
+    fi
+
+    if ! docker build -f Dockerfile -t us-central1-docker.pkg.dev/"${projectId}"/"$repositoryName"/"$artifactName":"$tag" .; then
         echo "Failed to build Docker image."
         sendMessage "$replyTopic" "Pipeline failed."
         exit 1
     fi
 
-    if ! docker push us-central1-docker.pkg.dev/"${projectId}"/"$repositoryName"/"$artifactName":0.1; then
-        echo "Failed to push Docker image."
-        sendMessage "$replyTopic" "Pipeline failed."
-        exit 1
-    fi
+    if [[ $doNotPush == true ]]; then
+        echo "Docker image us-central1-docker.pkg.dev/${projectId}/$repositoryName/$artifactName:$tag built successfully - did not push because we are not on main or release."
+    else
+        if ! docker push us-central1-docker.pkg.dev/"${projectId}"/"$repositoryName"/"$artifactName":"$tag"; then
+            echo "Failed to push Docker image."
+            sendMessage "$replyTopic" "Pipeline failed."
+            exit 1
+        fi
 
-    echo "Docker image built and pushed successfully."
+        echo "Docker image us-central1-docker.pkg.dev/${projectId}/$repositoryName/$artifactName:$tag built and pushed successfully."
+    fi
 }
